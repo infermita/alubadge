37780ab06eeb95de450131a3ff42c5cbad480813
infermita <alberto@infermita.homepc.it>
2017-11-06 18:29:07 +0100

    Flick led lcd ba

--- lib/nfcthread.cpp
+++ lib/nfcthread.cpp
@@ -83,7 +83,7 @@ void NfcThread::run(){
                              output &=~ (1<<3);
                              on = 1;
                          }
-                         wLcd->write_word(output);
+                         wLcd->send_command(output);
                          usleep(100000);
 
                      }
